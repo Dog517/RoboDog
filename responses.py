a982747372247c8200b17360fe43fcd60429a515
Dog517 <102268965+Dog517@users.noreply.github.com>
2023-12-06 23:24:36 -0500

    bot

---
+++ responses.py
@@ -0,0 +1,13 @@
+import discord
+authorusername= ""
+
+def handle_response(message) -> str:
+    p_message = message.lower()
+    if 'gun' in p_message:
+        return 'Murica!'
+    if 'play' in p_message and 'football' in p_message and authorusername == 'moroccandudelmao':
+        return 'Its soccer, you Moroccan!'
+    if p_message == 'Make fun of morocco robodog':
+        print('p_message check for morocco succeded! morocco if check #1 says' + authorusername)
+        if authorusername == 'thedog51gamer':
+            return "Oh, for fuck's sake, talking about Moroccan, the gaming genius from the land of dust and camels. This dude thinks he's the Sultan of the joystick, but in reality, he's just a sorry excuse for a gamer. I mean, seriously, Morocco, the land of vibrant culture and historic cities, and this moron is the best they can offer in the gaming department? What a fucking embarrassment. First off, let's talk about his skills – or lack thereof. It's like watching a blindfolded toddler trying to find its way through a maze. I've seen potatoes with more strategic thinking than this numbnut. And don't even get me started on his reflexes; they're slower than a snail on tranquilizers. Maybe he's too busy riding his fucking camel to bother improving. Now, the name – Moroccan. Real creative, buddy. Did your brain freeze in the Sahara heat, or are you just too unimaginative to come up with something remotely interesting? I bet his creativity level is on par with a dead fish. 'Moroccan,' really? Might as well call yourself 'Bland-african' for all the excitement your presence brings. And his online demeanor? Holy shit, it's like dealing with a hormonal teenager who just discovered the internet. Every sentence he types is a painful reminder of why some people should never be allowed near a keyboard. I'd rather listen to nails on a chalkboard than endure his inane chatter. So, Robodog, making fun of Moroccan is like shooting fish in a barrel. The guy is a walking, talking joke. I wouldn't be surprised if he thinks the Casablanca movie is a documentary about his epic gaming journey. Get your shit together, Moroccan, or better yet, stick to something you're good at.. like counting grains of sand in the Sahara."
